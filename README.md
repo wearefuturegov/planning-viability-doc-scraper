82b9d463be0b58696973c96f97e151eed9b8ebb4
Adam Rae <262674+raemond@users.noreply.github.com>
2019-10-15 18:08:14 +0100

    Created Tower Hamlets variant spider

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Project: Planning Viability
 
 ## Deploy Instructions
 `virtualenv .env && source .env/bin/activate && pip install -r requirements.txt`
+
+## Running the crawler
+`scrapy crawl viabilityDocsSouthwark -a urlsFile=docListing_all.csv -a outputPDFs=../idoxViabilityScraper/pdfs -a outputCSVs=../idoxViabilityScraper/csvs`
---
+++ idoxViabilityScraper/idoxViabilityScraper/spiders/ViabilityDocsSpiderTowerHamlets.py
@@ -0,0 +1,61 @@
+import scrapy
+import csv
+import wget
+import sys
+
+class ViabilityDocsSpiderTowerHamlets(scrapy.Spider):
+	name = "viabilityDocsTowerHamlets"
+	
+	def start_requests(self):
+		try:
+			urlsFile = self.urlsFile
+		except:
+			print("ERROR: URL file argument not set!")
+			sys.exit(1)
+			
+		try:
+			outputPDFs = self.outputPDFs
+		except:
+			print("ERROR: Output PDF location not set!")
+			sys.exit(1)
+			
+		try:
+			outputCSVs = self.outputCSVs
+		except:
+			print("ERROR: Output CSV location not set!")
+			sys.exit(1)
+			
+		url_header = "https://development.towerhamlets.gov.uk/online-applications/applicationDetails.do?activeTab=documents&keyVal=DCAPR_"
+		urls_all = []
+		with open(self.urlsFile, 'r') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+			for row in reader:
+					urls_all.append(url_header + row[1])
+
+		urls = urls_all
+		for i, url in enumerate(urls):
+			yield scrapy.Request(url=url, meta={'cookiejar': i}, callback=self.parseApplicationPage)
+
+
+	def parseApplicationPage(self, response):
+		page = response.url.split("=")[-1]
+		filename = '/THdocListingNew-%s.csv' % page
+		filename = self.outputCSVs + filename
+		items = response.xpath("//*[@id='Documents']/tr[td//text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'viability')]]/td[7]/a/@href").getall() # using XPath 1.0 hack for matching case insensitive
+		with open(filename, 'w') as f:
+			for item in items:
+				f.write("%s,%s,https://development.towerhamlets.gov.uk%s\n" % (page, response.url, item))
+				url = "https://development.towerhamlets.gov.uk"+item
+				print("URL: "+url)
+				yield scrapy.Request(url=url, meta={'cookiejar': response.meta['cookiejar']}, callback=self.retrieveViabilityDoc)
+		self.log('Saved file %s' % filename)
+		
+	def retrieveViabilityDoc(self, response):
+		page = response.url.split("/")[-1]
+		filename = '/THdocListingNew-%s' % page
+		filename = self.outputPDFs+filename
+		print("INFO: Filename = %s" % filename)
+		pdf = response.body
+		with open(filename, 'wb') as f:
+			f.write(pdf)
+		self.log('LOG: Saved viability doc')
